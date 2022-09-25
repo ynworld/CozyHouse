eb63677159b659efc181950a748a43b5a6147ff2
Yura <ynworld85@mac.com>
2022-09-25 13:54:26 -0400

    fix: use css to style mobile navigation

--- pages/main/script.js
+++ pages/main/script.js
@@ -7,17 +7,12 @@ navIcon.addEventListener("click", function () {
   if (!isMenuOpen) {
     navigation.style.display = "flex";
     navIcon.style.transform = "rotate(90deg)";
-    header.style.position = "fixed";
-    header.style.width = "46rem";
-    header.style.height = "100%";
-    header.style.background = "var(--color-grey-dark-3)";
+    header.classList.add("nav--open");
     isMenuOpen = true;
   } else {
     navigation.style.display = "none";
     navIcon.style.transform = "rotate(0deg)";
-    header.style.position = "relative";
-    header.style.width = "100%";
-    header.style.background = "transparent";
+    header.classList.remove("nav--open");
     isMenuOpen = false;
   }
 });
