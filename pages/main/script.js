44d58232f8feb43da8bab7eaf22ab6fe4fbf4bfc
Yura <ynworld85@mac.com>
2022-09-24 21:00:24 -0400

    feat: add collapsable navigation

---
+++ pages/main/script.js
@@ -0,0 +1,23 @@
+const navIcon = document.querySelector(".navigation__icon-block");
+const navigation = document.querySelector(".nav");
+const header = document.querySelector(".header");
+let menuClosed = true;
+
+navIcon.addEventListener("click", function () {
+  if (menuClosed) {
+    navigation.style.display = "flex";
+    navIcon.style.transform = "rotate(90deg)";
+    header.style.position = "fixed";
+    header.style.width = "46rem";
+    header.style.height = "100%";
+    header.style.background = "var(--color-grey-dark-3)";
+    menuClosed = false;
+  } else {
+    navigation.style.display = "none";
+    navIcon.style.transform = "rotate(0deg)";
+    header.style.position = "relative";
+    header.style.width = "100%";
+    header.style.background = "transparent";
+    menuClosed = true;
+  }
+});
