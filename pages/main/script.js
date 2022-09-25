cfe508c5549a45b6563fdc5028c35230f731cc1b
Yura <ynworld85@mac.com>
2022-09-25 13:19:02 -0400

    fix: correct boolean variable name

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,23 +1,23 @@
 const navIcon = document.querySelector(".navigation__icon-block");
 const navigation = document.querySelector(".nav");
 const header = document.querySelector(".header");
-let isMenuOpen = true;
+let isMenuOpen = false;
 
 navIcon.addEventListener("click", function () {
-  if (menuClosed) {
+  if (!isMenuOpen) {
     navigation.style.display = "flex";
     navIcon.style.transform = "rotate(90deg)";
     header.style.position = "fixed";
     header.style.width = "46rem";
     header.style.height = "100%";
     header.style.background = "var(--color-grey-dark-3)";
-    menuClosed = false;
+    isMenuOpen = true;
   } else {
     navigation.style.display = "none";
     navIcon.style.transform = "rotate(0deg)";
     header.style.position = "relative";
     header.style.width = "100%";
     header.style.background = "transparent";
-    menuClosed = true;
+    isMenuOpen = false;
   }
 });
