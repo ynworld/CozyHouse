917b88c53bd5ed7deab3a0f091d4789331821753
Yura <ynworld85@mac.com>
2022-09-25 22:46:39 -0400

    chore: configure prettier single quotes for js only

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,18 +1,18 @@
-const navIcon = document.querySelector(".navigation__icon-block");
-const navigation = document.querySelector(".nav");
-const header = document.querySelector(".header");
+const navIcon = document.querySelector('.navigation__icon-block');
+const navigation = document.querySelector('.nav');
+const header = document.querySelector('.header');
 let isMenuOpen = false;
 
-navIcon.addEventListener("click", function () {
+navIcon.addEventListener('click', function () {
   if (!isMenuOpen) {
-    navigation.style.display = "flex";
-    navIcon.style.transform = "rotate(90deg)";
-    header.classList.add("nav--open");
+    navigation.style.display = 'flex';
+    navIcon.style.transform = 'rotate(90deg)';
+    header.classList.add('nav--open');
     isMenuOpen = true;
   } else {
-    navigation.style.display = "none";
-    navIcon.style.transform = "rotate(0deg)";
-    header.classList.remove("nav--open");
+    navigation.style.display = 'none';
+    navIcon.style.transform = 'rotate(0deg)';
+    header.classList.remove('nav--open');
     isMenuOpen = false;
   }
 });
